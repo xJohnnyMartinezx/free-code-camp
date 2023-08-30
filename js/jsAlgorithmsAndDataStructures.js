b614ee4c2347971aaf4c8f6d0989c04ca5ab2265
Johnny <mrtnz.jn@gmail.com>
2023-08-29 19:57:46 -0500

    completed string immutability exercise

--- js/jsAlgorithmsAndDataStructures.js
+++ js/jsAlgorithmsAndDataStructures.js
@@ -36,8 +36,14 @@
 
 
     //     **********************************
+    // Correct the assignment to myStr, so it contains the string value of Hello World using the approach shown in the example above.
+    // Setup
+    let myStr = "Jello World";
 
-
+// Only change code below this line
+    myStr = "Hello World"; // Change this line
+    console.log(myStr);
+// Only change code above this line
 
 
     //     **********************************
