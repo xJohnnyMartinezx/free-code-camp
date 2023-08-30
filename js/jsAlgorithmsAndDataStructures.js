a31793e4c319fc6bd3a21d66234b9ab3ca46fbe0
Johnny <mrtnz.jn@gmail.com>
2023-08-29 19:52:42 -0500

    completed bracket notation exercise

---
+++ js/jsAlgorithmsAndDataStructures.js
@@ -0,0 +1,78 @@
+"use strict";
+(function(){
+
+//     // Appending Variables to Strings
+//
+//     // Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+//     // Change code below this line
+//     const someAdjective = "cool.";
+//     let myStr = "Learning to code is ";
+//
+//     myStr += someAdjective;
+//     console.log(myStr);
+//
+// //     **********************************
+// //     Find the Length of a String
+// //     Use the .length property to set lastNameLength to the number of characters in lastName.
+//         // Setup
+//     let lastNameLength = 0;
+//     const lastName = "Lovelace";
+//
+//         // Only change code below this line
+//     lastNameLength = lastName.length;
+//     console.log(lastNameLength);
+
+
+    //     **********************************
+    // Use Bracket Notation to Find the First Character in a String
+    // Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+        // Setup
+    let firstLetterOfLastName = "";
+    const lastName = "Lovelace";
+
+        // Only change code below this line
+    firstLetterOfLastName = lastName[0]; // Change this line
+    console.log(firstLetterOfLastName);
+
+
+    //     **********************************
+
+
+
+
+    //     **********************************
+
+
+
+
+    //     **********************************
+
+
+
+    //     **********************************
+
+
+    //     **********************************
+
+
+
+    //     **********************************
+
+
+
+    //     **********************************
+
+
+
+    //     **********************************
+
+
+
+
+
+
+
+
+
+
+})();
